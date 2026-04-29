d0ca19cb1149027d6e945ce5502644da3943022b
Deign Grey Lazaro <deign88@protonmail.com>
2026-04-30 01:45:20 +0800

    Revert to max-w-4xl container

--- src/components/des/DESController.tsx
+++ src/components/des/DESController.tsx
@@ -96,7 +96,7 @@ export default function DESController({ trace, currentStep, steps, totalSteps }:
   };
 
   return (
-    <div className="max-w-7xl mx-auto space-y-6">
+    <div className="max-w-4xl mx-auto space-y-6">
       {renderStepContent()}
     </div>
   );
